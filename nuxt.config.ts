6db6b51b2d840782fb6d777608accb1a814d7965
finxol <git@finxol.io>
2024-12-11 14:51:44 +0100

    chore: add compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,24 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    telemetry: false,
-    css: [
-        '~/assets/css/main.scss',
-    ],
-    app: {
-        head: {
-            title: 'Colin Ozanne',
-            meta: [
-                {charset: 'utf-8'},
-                {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-                {hid: 'description', name: 'description', content: 'Colin Ozanne\'s personal portfolio' },
-            ],
-            htmlAttrs: {
-                lang: 'en',
-            }
-        },
-    }
+  telemetry: false,
+
+  css: [
+      '~/assets/css/main.scss',
+  ],
+
+  app: {
+      head: {
+          title: 'Colin Ozanne',
+          meta: [
+              {charset: 'utf-8'},
+              {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+              {hid: 'description', name: 'description', content: 'Colin Ozanne\'s personal portfolio' },
+          ],
+          htmlAttrs: {
+              lang: 'en',
+          }
+      },
+  },
+
+  compatibilityDate: '2024-12-11'
 })
