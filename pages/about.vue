312d0d0ca9acbfed917a17e65b405111e0973699
finxol <git@finxol.io>
2025-03-24 17:43:26 +0100

    feat: mov eabout text to about page + remove tech skills

--- pages/about.vue
+++ pages/about.vue
@@ -1,115 +1,79 @@
 <script setup lang="ts">
-const skills = [
-    {
-        name: 'HTML / CSS',
-        icon: 'fa-brands:html5',
-        level: 4.5,
-    },
-    {
-        name: 'TypeScript',
-        icon: 'fa-brands:js',
-        level: 4.5,
-    },
-    {
-        name: 'Nuxt',
-        icon: 'fa-brands:vuejs',
-        level: 4,
-    },
-    {
-        name: 'Node',
-        icon: 'fa-brands:node-js',
-        level: 4.5,
-    },
-    {
-        name: 'Express',
-        icon: 'fa-brands:node-js',
-        level: 4.5,
-    },
-    {
-        name: 'Git',
-        icon: 'fa-brands:git-alt',
-        level: 3.5,
-    },
-    {
-        name: 'Docker',
-        icon: 'fa-brands:docker',
-        level: 3.5,
-    },
-    {
-        name: 'Linux',
-        icon: 'fa-brands:linux',
-        level: 4,
-    },
-    {
-        name: 'Rust',
-        icon: 'fa-brands:rust',
-        level: 1,
-    },
-    {
-        name: 'Python',
-        icon: 'fa-brands:python',
-        level: 3.5,
-    }
-]
 
-const lowerStar = (level: number) => {
-    return Math.floor(level)
-}
-
-const halfStar = (level: number) => {
-    return level % 1 >= 0.5
-}
-
-const upperStar = (level: number) => {
-    return Math.ceil(level)
-}
 </script>
 
 <template>
-    <section>
+
+    <section id="about">
+        <p>
+            Hiya! 👋
+        </p>
+        <p>
+            I'm a computer science student and entrepreneur from 🇫🇷 France.
+        </p>
+        <p>
+        I natively speak 🇬🇧 <strong>English</strong> and 🇫🇷 <strong>French</strong>.
+        </p>
+        <p>
+            Some of my interests include:
+            <strong>web development</strong>,
+            <strong>infosec</strong>,
+            <strong>cybersecurity</strong>
+            and <strong>systems administration</strong>.
+        </p>
+        <p>
+            I'm currently working on <a href="https://github.com/finxol/karr" target="_blank" rel="noreferrer noopener">Karr</a>,
+            a federated carpooling platform for businesses.
+            <br>
+            You can find my other most interesting projects just <NuxtLink to="/projects">next door</NuxtLink>.
+        </p>
+        <p>
+            I regularly take part in CTFs and other cybersecurity events & conferences.
+        </p>
+        <p>
+            Feel free to use my <NuxtLink to="/contact">contact details</NuxtLink> if you wish to get in touch.
+        </p>
+
+        <small>
+            Please note that this website is still a work in progress.
+        </small>
+    </section>
+
+    <section
+        id="ed-exp"
+    >
         <div
             id="container"
         >
             <div
-                id="skills"
+                id="education"
             >
-                <h1>Skills</h1>
+                <h1>Education</h1>
                 <div
-                    id="skills-container"
+                    id="education-container"
                 >
                     <div
-                        v-for="skill in skills"
-                        :key="skill.name"
-                        class="skill"
+                        class="education-element"
                     >
-                        <span>
-                            <Icon
-                                :name="skill.icon"
-                                size="1em"
-                            />
-                        </span>
-                        <span>
-                            {{ skill.name }}
-                        </span>
-                        <span>
-                            <Icon
-                                name="fa6-solid:star"
-                                v-for="i in lowerStar(skill.level)"
-                                :key="i"
-                                size="1em"
-                            />
-                            <Icon
-                                name="fa6-solid:star-half-stroke"
-                                v-if="halfStar(skill.level)"
-                                size="1em"
-                            />
-                            <Icon
-                                name="fa6-regular:star"
-                                v-for="i in (5 - upperStar(skill.level))"
-                                :key="i"
-                                size="1em"
-                            />
-                        </span>
+                        <h2>ISTIC, Université de Rennes, France</h2>
+                        <h3>Licence Informatique</h3>
+                        <p>2024-2025</p>
+                    </div>
+
+                    <div
+                        class="education-element"
+                    >
+                        <h2>UEF, Finland</h2>
+                        <h3>Erasmus semester</h3>
+                        <p>2023</p>
+                    </div>
+
+                    <div
+                        class="education-element"
+                    >
+                        <h2>IUT Vannes</h2>
+                        <h3>BUT, Computer Science</h3>
+                        <p>2021-2023</p>
                     </div>
                 </div>
             </div>
@@ -118,9 +82,8 @@ const upperStar = (level: number) => {
                 id="experiences"
             >
                 <h1>Experience</h1>
-
                 <p>
-                    Work in progress...
+                    VisCircle
                 </p>
             </div>
         </div>
@@ -128,17 +91,40 @@ const upperStar = (level: number) => {
 </template>
 
 <style scoped lang="scss">
-section {
-    min-height: calc(100% - 2rem);
-    width: calc(100% - 2rem);
-    margin: 0 auto 2rem;
+section#about {
+    min-height: 83%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: clamp(.5rem, 2vh, 1rem);
+    width: 70%;
+    margin: 0 auto 4rem;
+    padding-top: 1rem;
+    font-size: 1.5rem;
 
-    h1 {
-        margin: 0;
-        padding-top: 3rem;
-        font-size: 3rem;
-        text-align: center;
+    @media (max-width: 768px) {
+        width: 80%;
+        margin: 2rem auto 5rem;
     }
+
+    p {
+        margin: 0;
+        text-align: left;
+    }
+
+    small {
+        margin-top: 3rem;
+        font-size: 1rem;
+        font-style: italic;
+    }
+}
+
+section#ed-exp {
+    //min-height: calc(100% - 2rem);
+    width: calc(100% - 2rem);
+    margin: 0 auto 8rem;
+
 
     div#container {
         display: grid;
@@ -146,7 +132,7 @@ section {
         grid-template-rows: 1fr;
         gap: 1rem;
         justify-content: center;
-        align-items: center;
+        align-items: flex-start;
         width: 100%;
         min-height: 100%;
         margin: 1rem 0;
@@ -156,77 +142,88 @@ section {
             grid-template-rows: 1fr 1fr;
         }
 
-        div#skills {
+        h1 {
+            margin: 0;
+            padding-top: 3rem;
+            font-size: 3rem;
+            text-align: center;
+        }
+
+        div#education {
             height: 100%;
-            width: 100%;
+            width: calc(100% - 3rem);
+            margin-left: 3rem;
             border-right: 1px solid rgba(255, 255, 255, 0.1);
 
             @media (max-width: 768px) {
+                width: calc(100% - 1.5rem);
+                margin-left: 1.5rem;
                 border-right: none;
                 border-bottom: 1px solid rgba(255, 255, 255, 0.1);
             }
 
-            div#skills-container {
+            div#education-container {
                 display: flex;
-                flex-direction: row;
+                flex-direction: column;
                 flex-wrap: wrap;
                 gap: 1rem;
                 justify-content: center;
-                align-items: center;
+                align-items: flex-start;
                 margin-top: 2rem;
+
+                border-left: 1px solid rgba(255, 255, 255, 0.8);
 
                 @media (max-width: 768px) {
                     gap: 0.5rem;
                 }
 
-                div.skill {
+                div.education-element {
                     display: flex;
+                    position: relative;
                     flex-direction: column;
                     justify-content: center;
-                    align-items: center;
-                    width: 10rem;
-                    height: 9rem;
-                    padding: 0.5rem;
+                    align-items: flex-start;
+                    gap: 0.75rem;
+                    margin-left: 2rem;
+                    width: calc(90% - 2rem);
+                    padding: 1rem 1.5rem;
                     border: none;
                     border-radius: 0.5rem;
                     background-color: rgba(255, 255, 255, 0.1);
                     text-align: center;
 
-                    @media (max-width: 768px) {
-                        width: 7rem;
-                        height: 6.5rem;
+                    &:before {
+                        content: "";
+                        width: 1rem;
+                        height: 1rem;
+                        position: absolute;
+                        top: 1.5rem;
+                        left: -2.53rem;
+                        background-color: rgba(255, 255, 255, 1);
+                        border-radius: 0.5rem;
+
                     }
 
-                    span {
-                        width: 100%;
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
+                    h2, h3, p {
+                        margin: 0;
+                        padding: 0;
+                        text-align: left;
+                        font-size: 1rem;
+                        color: #f0f0f0;
                     }
 
-                    span:first-child {
-                        font-size: 2rem;
-
-                        @media (max-width: 768px) {
-                            font-size: 1.5rem;
-                        }
-                    }
-
-                    span:nth-child(2) {
+                    h2 {
                         font-size: 1.5rem;
-
-                        @media (max-width: 768px) {
-                            font-size: 0.9rem;
-                        }
+                        font-weight: bold;
                     }
 
-                    span:last-child {
-                        padding-top: .5rem;
-                        font-size: 1.1rem;
+                    h3 {
+                        font-size: 1.2rem;
+                        font-style: italic;
+                    }
 
-                        @media (max-width: 768px) {
-                            font-size: 0.7rem;
-                        }
+                    p {
+                        font-size: 0.8rem;
                     }
                 }
             }
--- pages/index.vue
+++ pages/index.vue
@@ -17,45 +17,12 @@
             </span>
             Computer Science Student
         </h3>
-        <a href="#about">
+        <NuxtLink href="/about">
             <span>
                 <Icon name="fa6-solid:user" />
             </span>
             About Me
-        </a>
-    </section>
-    <section id="about">
-        <p>
-            Hiya! 👋
-        </p>
-        <p>
-            I'm a computer science student from 🇫🇷 France.
-        </p>
-        <p>
-          I natively speak 🇬🇧 <strong>English</strong> and 🇫🇷 <strong>French</strong>.
-        </p>
-        <p>
-            Some of my interests include:
-            <strong>web development</strong>,
-            <strong>infosec</strong>,
-            <strong>cybersecurity</strong>
-            and <strong>systems administration</strong>.
-        </p>
-        <p>
-            I'm currently working on <a href="https://unicovoit.fr" target="_blank" rel="noreferrer noopener">UniCovoit</a>,
-            a carpooling platform for students;
-            but you can find my other most interesting projects just <NuxtLink to="/projects">next door</NuxtLink>.
-        </p>
-        <p>
-            I regularly take part in CTFs and other cybersecurity events & conferences.
-        </p>
-        <p>
-            Feel free to use my <NuxtLink to="/contact">contact details</NuxtLink> if you wish to get in touch.
-        </p>
-
-        <small>
-            Please note that this website is still a work in progress.
-        </small>
+        </NuxtLink>
     </section>
 </template>
 
@@ -108,38 +75,6 @@ section#home {
         box-shadow: 0 0 1rem .7rem rgba(0, 0, 0, .1);
         padding: 0 .5rem;
         border-radius: 1rem;
-    }
-}
-
-section#about {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: clamp(.5rem, 2vh, 1rem);
-    width: 70%;
-    margin: 0 auto 4rem;
-    padding-top: 1rem;
-    font-size: 1.5rem;
-
-    @media (max-width: 768px) {
-        width: 80%;
-        margin: 2rem auto 5rem;
-    }
-
-    p {
-        margin: 0;
-        text-align: justify;
-
-        @media (max-width: 768px) {
-            text-align: left;
-        }
-    }
-
-    small {
-        margin-top: 3rem;
-        font-size: 1rem;
-        font-style: italic;
     }
 }
 </style>
